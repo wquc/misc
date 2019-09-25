13615d3810047a9019974514e6225978c226ecae
qi-wang-uc <wangqi1900@gmail.com>
2019-09-24 22:05:20 -0400

    corrected NSTEP info

--- scripts/05-qatdcd.cpp
+++ scripts/05-qatdcd.cpp
@@ -18,14 +18,16 @@ using std::endl;
 using std::cerr;
 
 int main(int argc, char** argv) {
-    cout << "INFO> QATDCD version 1.0" << endl
+    cout << "INFO> QATDCD version 1.1" << endl
          << "INFO> This program does NOT check sanity of trajectory files." << endl
-         << "INFO> Merged trajectory will have same header as the first input except NFILE." << endl;
+         << "INFO> Merged trajectory will have same header as the first input except NFILE and NSTEP." << endl
+         << "INFO> --------------------------------" << endl;
     if (argc<3) {
         cerr << "ERROR> Not enough input arguments!" << endl;
         return 1;
     }
     int  frame_counter = 0;
+    int  step_counter  = 0;
     char dcd_head1[100];
     char dcd_title1[80];
     char dcd_title2[80];
@@ -46,6 +48,9 @@ int main(int argc, char** argv) {
         inp_file.read(dcd_title2, 80);
         inp_file.read(dcd_head2, 16);
         int n_file = *(int*)(&dcd_head1[8]);
+        int n_priv = *(int*)(&dcd_head1[12]);
+        int n_savc = *(int*)(&dcd_head1[16]);
+        int n_step = *(int*)(&dcd_head1[20]);
         int q_cell = *(int*)(&dcd_head1[48]);
         int n_atom = *(int*)(&dcd_head2[8]);
         int sz_frame = q_cell ? (3*(4*n_atom+8)+56) : (3*(4*n_atom+8));
@@ -60,10 +65,16 @@ int main(int argc, char** argv) {
         out_file.write(reinterpret_cast<char*>(&dcd_buffer[0]), sz_frame*n_file);
         inp_file.close();
         frame_counter += n_file;
+        step_counter  += n_step;
     }
     out_file.seekp(8, std::ios::beg);
     out_file.write(reinterpret_cast<const char*>(&frame_counter), 4);
-    cout << "INFO> Done. Overall number of frames: " << frame_counter << endl;
+    out_file.seekp(20, std::ios::beg);
+    out_file.write(reinterpret_cast<const char*>(&step_counter), 4);
+    cout << "INFO> --------------------------------" << endl
+         << "INFO> DCD merge completed." << endl
+         << "INFO> Overall number of frames: " << frame_counter << endl
+         << "INFO> Overall number of steps:  " << step_counter  << endl;
     out_file.close();
 
     return 0;
